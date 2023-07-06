e6f323aa4986166db67a816b49e8047703a618b7
Riccardo Reali <rreali@my.centennialcollege.ca>
2023-07-06 13:39:18 -0400

    update

--- src/components/Form1.tsx
+++ src/components/Form1.tsx
@@ -1,27 +1,49 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState, useEffect } from "react";
 import "../App.css";
 
 function Form1() {
   const titleRef = useRef<HTMLInputElement>(null);
   const dateRef = useRef<HTMLInputElement>(null);
   const categoryRef = useRef<HTMLInputElement>(null);
+  const [tableData, setTableData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("tableData");
+    if (storedData) {
+      setTableData(JSON.parse(storedData));
+    }
+  }, []);
+
+  const deleteTableData = (index: number) => {
+    const updatedData = [...tableData];
+    updatedData.splice(index, 1);
+    setTableData(updatedData);
+    localStorage.setItem("tableData", JSON.stringify(updatedData));
+  };
 
   const handleClickSubmit = (event: FormEvent) => {
     event.preventDefault();
 
     if (
-      titleRef.current &&
-      dateRef.current &&
-      categoryRef.current &&
-      titleRef.current.value &&
-      dateRef.current.value &&
-      categoryRef.current.value
+      titleRef.current?.value &&
+      dateRef.current?.value &&
+      categoryRef.current?.value
     ) {
       alert("Thanks for the input!");
-      // Reset input field values
-      titleRef.current.value = "";
-      dateRef.current.value = "";
-      categoryRef.current.value = "";
+
+      const newData = {
+        title: titleRef.current!.value,
+        date: dateRef.current!.value,
+        category: categoryRef.current!.value,
+      };
+
+      const updatedData = [...tableData, newData];
+      setTableData(updatedData);
+      localStorage.setItem("tableData", JSON.stringify(updatedData));
+
+      titleRef.current!.value = "";
+      dateRef.current!.value = "";
+      categoryRef.current!.value = "";
     } else {
       alert("You must fill in all the input fields.");
     }
@@ -71,6 +93,31 @@ function Form1() {
           Submit
         </button>
       </form>
+
+      {tableData.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Due Date</th>
+              <th>Category</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map((data, index) => (
+              <tr key={index}>
+                <td>{data.title}</td>
+                <td>{data.date}</td>
+                <td>{data.category}</td>
+                <td>
+                  <button onClick={() => deleteTableData(index)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
